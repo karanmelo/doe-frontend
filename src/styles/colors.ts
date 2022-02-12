ca8b933f9fa5da0e6afd84d2e728cf02cd688b05
Karan Melo <karan_melo@hotmail.com>
2022-02-12 17:51:16 -0300

    feat: create style guide

---
+++ src/styles/colors.ts
@@ -0,0 +1,33 @@
+export const colors = Object.freeze({
+  background: '#EDF1F3',
+  base: {
+    green: '#438B35',
+    blue: '#457BC3',
+    red: '#991C3B',
+  },
+  neutral: {
+    white: '#FFFFFF',
+    lightGray: '#F2F3F1',
+    darkGray: '#444545',
+    gray: '#E4E9EB',
+    black: '#202328',
+    darkGreen: '#384529',
+  },
+  functional: {
+    danger: '#C52330',
+    warning: '#F2D03A',
+    success: '#78B833',
+  },
+  shadow: {
+    primary:
+      '0px 2px 2px rgba(50, 50, 71, 0.06), 0px 2px 4px rgba(50, 50, 71, 0.06)',
+    secondary:
+      '0px 4px 4px rgba(50, 50, 71, 0.08), 0px 4px 8px rgba(50, 50, 71, 0.06)',
+    tertiary:
+      '0px 8px 8px rgba(50, 50, 71, 0.08), 0px 8px 16px rgba(50, 50, 71, 0.06)',
+    quaternary:
+      '0px 12px 12px rgba(50, 50, 71, 0.08), 0px 16px 24px rgba(50, 50, 71, 0.08)',
+    quinary:
+      '0px 16px 16px rgba(50, 50, 71, 0.12), 0px 24px 32px rgba(50, 50, 71, 0.16)',
+  },
+});
