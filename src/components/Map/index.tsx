8c64d4a0b178cea8852552bfb8e29c6a9fc9a282
Karan Melo <karan_melo@hotmail.com>
2022-02-12 21:58:21 -0300

    fix: extends map props from react-leaflet MapProps

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { LeafletMouseEvent, LatLngExpression } from 'leaflet';
 import React from 'react';
 
-import { Map as RLMap, TileLayer } from 'react-leaflet';
+import { Map as RLMap, TileLayer, MapProps as RLMapProps } from 'react-leaflet';
 
-export type MapProps = {
+export type MapProps = RLMapProps & {
   center?: LatLngExpression;
   zoom?: number | undefined;
   style?: React.CSSProperties | undefined;
@@ -16,8 +17,9 @@ export const Map: React.FC<MapProps> = ({
   zoom,
   style,
   onClick,
+  ...rest
 }) => (
-  <RLMap center={center} zoom={zoom} style={style} onClick={onClick}>
+  <RLMap center={center} zoom={zoom} style={style} onClick={onClick} {...rest}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
