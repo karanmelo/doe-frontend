f90f9410b3b4426def59455a7e55e406c3b1784a
Karan Melo <karan_melo@hotmail.com>
2022-02-12 22:00:00 -0300

    fix: update landing page style

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -5,26 +5,31 @@ import { Link } from 'react-router-dom';
 import '../../styles/pages/landing.css';
 
 import logoImg from '../../images/logo.svg';
+import mascoteDoisImg from '../../images/mascote-2.svg';
 
 export const Landing = () => {
   return (
     <div id="page-landing">
       <div className="content-wrapper">
-        <img src={logoImg} alt="DOE" />
-
-        <main>
-          <h1>Distribua amor e vida a quem precisa</h1>
-          <p>Visite um centro de doação de sangue e salve vidas</p>
-        </main>
-
-        <div className="location">
-          <strong>Salvador</strong>
-          <span>Bahia</span>
+        <div className="header">
+          <img src={logoImg} alt="DOE" />
+          <div className="location">
+            <strong>Salvador</strong>
+            <span>Bahia</span>
+          </div>
         </div>
 
-        <Link to="/app" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </Link>
+        <div className="content">
+          <h1>Distribua amor e vida a quem precisa</h1>
+          <img width="479" height="356" src={mascoteDoisImg} alt="DOE" />
+        </div>
+
+        <div className="footer">
+          <p>Visite um centro de doação de sangue e salve vidas</p>
+          <Link to="/app" className="enter-app">
+            <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+          </Link>
+        </div>
       </div>
     </div>
   );
