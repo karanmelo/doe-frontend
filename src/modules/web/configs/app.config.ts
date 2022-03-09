dea140f270168059a046acde2ee6d1c0fbf52d57
Karan Melo <karan_melo@hotmail.com>
2022-03-09 14:35:56 -0300

    feat: adding apiClientId to app config

--- src/modules/web/configs/app.config.ts
+++ src/modules/web/configs/app.config.ts
@@ -1,4 +1,5 @@
 export const appConfig = {
   useMock: process.env.REACT_APP_USE_MOCK || 'false',
   apiBaseUrl: process.env.REACT_APP_BASE_URL || 'http://localhost:3003',
+  apiClientId: process.env.REACT_APP_API_CLIENT_ID,
 };
