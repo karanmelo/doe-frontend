e82c35e991b2e0b2ed5d36bb3829ba3bc8d92cbb
Karan Melo <karan_melo@hotmail.com>
2022-03-13 09:23:56 -0300

    fix: update HttpRequestErrorInterceptor rule

--- src/modules/web/services/HttpRequestErrorInterceptor.tsx
+++ src/modules/web/services/HttpRequestErrorInterceptor.tsx
@@ -1,36 +1,34 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef } from 'react';
 
 import { useHistory } from 'react-router-dom';
 
 import { api } from '.';
-import { useAuthContext } from '../stores';
+import { useAuth } from '../hooks';
 
 export const HttpRequestErrorInterceptor = () => {
   const interceptorId = useRef<number | undefined>(undefined);
 
   const history = useHistory();
-  const { signOut } = useAuthContext();
+  const { signOutProvider } = useAuth();
+
+  interceptorId.current = api.interceptors.response.use(undefined, (error) => {
+    signOutProvider();
+    switch (error.response.status) {
+      case 404:
+        history.push('/404');
+        break;
+      default:
+        history.push('/login');
+    }
+  });
 
   useEffect(() => {
-    interceptorId.current = api.interceptors.response.use(
-      undefined,
-      (error) => {
-        signOut();
-        switch (error.response.status) {
-          case 404:
-            history.push('/404');
-            break;
-          default:
-            history.push('/login');
-        }
-      }
-    );
-
     return () => {
       if (interceptorId.current)
         api.interceptors.response.eject(interceptorId.current);
     };
-  }, [history, signOut]);
+  }, [history]);
 
   return null;
 };
